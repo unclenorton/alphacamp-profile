18fc8f81d4ba9308c5b74ab5a9eb78f83507d33a
Dmytro Kharchenko <unclenorton@gmail.com>
2018-05-22 22:57:51 +0800

    Added javascript

---
+++ js/main.js
@@ -0,0 +1,28 @@
+(function(){
+  "use strict";
+  /* Start of your code */
+
+  console.log('The script is running.');
+
+  // Define the elements that we're going to manipulate
+  let navigationLinks = document.querySelectorAll('.nav-link');
+  let navbarElement = document.querySelector('.navbar');
+  let headerElement = document.querySelector('#header');
+
+  // Define the colours
+  let colorArray = ['bg-dark', 'bg-primary', 'bg-warning', 'bg-success', 'bg-error'];
+
+  function changeNavbarColor(colorClass) {  	
+  	navbarElement.classList.remove('bg-dark'); // Disable the default colour
+  	headerElement.className = colorArray[colorClass];
+  }
+
+  // Assign the event handler to all the links
+  for (let i = 0; i < navigationLinks.length; i++) {
+  	navigationLinks[i].onclick = function () {
+  		changeNavbarColor(i);
+  	}
+  }
+
+  /* End of your code */
+})();
